b595dff712bfb03d2725c6c4c41dd12705b379ea
Lucas <cont.lucas@gmail.com>
2023-12-22 19:52:24 -0300

    mini refactor

--- src/componentes/SearchComponents.jsx
+++ src/componentes/SearchComponents.jsx
@@ -1,45 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import { useState, useCallback } from "react";
 
-const SearchComponent = () => {
-  
-  const [ users, setUsers ] = useState([])
-  const [ search, setSearch ] = useState("")
-
-
-  const URL = 'https://rickandmortyapi.com'
-  
-
-  const showData = async () => {
-    const response = await fetch(URL)
-    const data = await response.json()
-    
-    setUsers(data)
-  }   
-   
-  const searcher = (e) => {
-      setSearch(e.target.value)   
-  }
-   
-   const results = !search ? users : users.filter((dato)=> dato.name.toLowerCase().includes(search.toLocaleLowerCase()))
-  
-   useEffect( ()=> {
-    showData()
+// eslint-disable-next-line react/prop-types
+export function SearchComponent({ onConfirmSearch }) {
+	const [search, setSearch] = useState("");
+  const onChange = useCallback((e) => {
+    setSearch(e.target.value);
   }, [])
-  
-  return (
-    <div>
-        <input value={search} onChange={searcher} type="text" placeholder='Busqueda' className='form-control'/>
-        <table className='table table-striped table-hover mt-4 shadow-lg'>
-            <tbody>
-                { results.map( (user) => (
-                    <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.username}</td>
-                    </tr>                    
-                ))}
-            </tbody>
-        </table>
-    </div>
-  )
+
+	const onSubmit = useCallback((e) => {
+		e.preventDefault();
+    onConfirmSearch(search)
+	}, [search, onConfirmSearch]);
+
+	return (
+			<form onSubmit={onSubmit}>
+				<input
+					value={search}
+					type="text"
+					placeholder="Search"
+					className="form-control"
+					onChange={onChange}
+				/>
+			</form>
+	);
 }
-export default SearchComponent
